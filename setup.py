4d51d09817424d611326ae1f2f321d90a1b5c35e
Rodrigo Valin <licorna@gmail.com>
2020-11-18 21:19:42 +0000

    Proper directory structure.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup, find_packages
+from distutils.core import setup
 
 setup(name='sonar',
       version='0.0.1',
@@ -8,5 +8,5 @@ setup(name='sonar',
       author='Rodrigo Valin',
       author_email='licorna@gmail.com',
       url='https://gitlab.com/licorna/sonar',
-      packages=find_packages(),
+      packages=["sonar"],
      )
