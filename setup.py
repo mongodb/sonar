7ddbf908e506cc065aabe9bd9f97dde595fbcc6c
Rodrigo Valin <licorna@gmail.com>
2020-11-20 16:06:02 +0000

    Fix setup.py file and modules.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='sonar',
-      version='0.0.1',
-      description='Sonar Docker Building Tools',
-      author='Rodrigo Valin',
-      author_email='licorna@gmail.com',
-      url='https://gitlab.com/licorna/sonar',
-      packages=["sonar"],
-     )
+setup(
+    name="sonar",
+    version="0.0.1",
+    description="Sonar Docker Building Tools",
+    author="Rodrigo Valin",
+    author_email="licorna@gmail.com",
+    url="https://gitlab.com/licorna/sonar",
+    packages=["sonar", "sonar.builders"],
+)
