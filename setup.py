745aea6d8ead8918c3584062dc0dd665b7e13495
Rodrigo Valin <rodrigo.valin@mongodb.com>
2021-01-08 06:57:45 -0300

    Bumped version to 0.0.5. (#12)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sonar",
-    version="0.0.4",
+    version="0.0.5",
     description="Sonar Docker Building Tools",
     author="Rodrigo Valin",
     author_email="rodrigo.valin@mongodb.com",
